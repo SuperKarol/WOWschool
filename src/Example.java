24cd6fa0cae59aa1a39e39c5e3c54532c4b2996c
Karol <karol.sawi9090@gmail.com>
2015-11-21 15:29:59 +0100

    zmiany

--- src/Example.java
+++ src/Example.java
@@ -1,4 +1,11 @@
 
 public class Example {
-
+	private int myIntegerField;
+	
+	public void display () {
+		System.out.println("My field: " + myIntegerField);
+	}
+	public Example() {
+		myIntegerField = 0;
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 /**
  * 
  * @author sova
- *
  */
 public class Main {
-	private static void display(String inpMessage) {		
+	public static void main(String[] args){	
+		Example localExampleleVariable0 = new Example();
+		Example localExampleleVariable1 = new Example();
+		
+		localExampleleVariable0.display();
+		localExampleleVariable1.display();
 	}
 }
